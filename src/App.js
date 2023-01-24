e0015e6136cc8cbc6f9fd3c80a135fdb6b451812
Kalkidan34 <kalkidnwoldemariam168@gmail>
2023-01-24 11:00:13 -0800

    NETFILIX first virgen

--- src/App.js
+++ src/App.js
@@ -1,25 +1,56 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Banner from './Banner';
+import Nav from './Nav';
+import requests from './requests';
+import Row from './Row';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav/>
+    <Banner/>
+     <Row title="NETFLIX ORIGINALS"
+     fetchUrl={requests.fetchNetflixOriginals}
+    isLargeRow
+    />
+     <Row title="Trending Now"
+     fetchUrl={requests.fetchTrending}/>
+     <Row title="Action Movies"
+     fetchUrl={requests.fetchActionMovies}/>
+      <Row title="Comedy Movies"
+     fetchUrl={requests.fetchComedyMovies}/>
+ <Row title="Horror Movies"
+     fetchUrl={requests.fetchHorrorMovies}/>
+      <Row title="Documentary Movies"
+     fetchUrl={requests.fetchDocumentaries}/>
+
+<Row title="Romance Movies"
+     fetchUrl={requests.fetchRomanceMovies}/>
+      <Row title="TopRated Movies"
+     fetchUrl={requests.fetchTopRatedMovies}/>
+
+
+
+
     </div>
   );
 }
 
 export default App;
+
+
+
+
+
+
+
+// Example API Request
+// https://api.themoviedb.org/3/movie/550?api_key=55d84a9100d1ecc652b340df5adf3a5f
+
+
+// https://api.themoviedb.org/3
+
+// API Key (v3 auth)
+
+// 55d84a9100d1ecc652b340df5adf3a5f
